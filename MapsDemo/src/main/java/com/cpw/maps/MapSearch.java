59196fa6b52fe6825e5df1a4fc71bbb948319e43
常晓 <1589273884@qq.com>
2020-03-15 16:44:23 +0800

    static block

--- MapsDemo/src/main/java/com/cpw/maps/MapSearch.java
+++ MapsDemo/src/main/java/com/cpw/maps/MapSearch.java
@@ -3,11 +3,17 @@ package com.cpw.maps;
 import com.cpw.entry.Person;
 
 public class MapSearch {
+    public  static String name;
+    static {
+        name="zs";
+    }
+
     public static void main(String[] args) {
         new Person();
         float f =    1 /3f ;
         Double d =  (double) 1 /3 ;
         System.out.println(f);
         System.out.println(d);
+        System.out.println(name);
     }
 }
