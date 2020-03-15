340406984d2b56fde42b77f2564b7ae01a816e3d
常晓 <1589273884@qq.com>
2020-03-15 15:39:48 +0800

    init demo workspace by maven

---
+++ MapsDemo/src/main/java/com/cpw/maps/MapSearch.java
@@ -0,0 +1,13 @@
+package com.cpw.maps;
+
+import com.cpw.entry.Person;
+
+public class MapSearch {
+    public static void main(String[] args) {
+        new Person();
+        float f =    1 /3f ;
+        Double d =  (double) 1 /3 ;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
---
+++ comm/src/main/java/com/cpw/entry/Person.java
@@ -0,0 +1,17 @@
+package com.cpw.entry;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Data
+public class Person {
+    private  String name;
+    private  String age;
+    private  String address;
+    private  String sex;
+
+}
