c7620a339be1033a3e4a521f2a05f64433613c3c
常晓 <1589273884@qq.com>
2020-04-01 13:14:58 +0800

    枚举实现单例

--- comm/src/main/java/com/cpw/entry/Person.java
+++ comm/src/main/java/com/cpw/entry/Person.java
@@ -13,5 +13,16 @@ public class Person {
     private  String age;
     private  String address;
     private  String sex;
+    static enum SingletonEnum{
+        INSTANCE;
+        private Person person;
 
+        private SingletonEnum() {
+            person=new Person();
+        }
+        public Person getPersonInstance(){
+            return person;
+        }
+    }
+    public  static Person getInstance(){return SingletonEnum.INSTANCE.getPersonInstance();}
 }
